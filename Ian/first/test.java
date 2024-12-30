e5931427a6582d50434e491d73714feb881668c0
Ian <ianchen611@gmail.cim>
2024-12-30 16:03:16 +0000

    Ian's first example in dir Ian

--- Ian/first/test.java
+++ Ian/first/test.java
@@ -1,7 +1,8 @@
-package first;
-
 public class test{
    public static void main(String[] args){
       System.out.println("Hello.");
+      for(int i=0;i<5;i++){
+         System.out.println("i is " + i);
+      }
    } 
 }
