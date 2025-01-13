ba5c7ed4da9e7490e73db092adfe8d43f5f57aa6
Du-Brandon <asdffhnkl123@gmail.com>
2025-01-13 17:45:53 +0800

    Add class_def

---
+++ Note/class_def/app.java
@@ -0,0 +1,17 @@
+class Test1{
+    static int a = 10;
+    static String b = "Br";
+
+}
+
+class Test2 {
+    static double a = 8964;
+    
+    public static void main(String[] args) {
+        System.out.println(Test1.a);
+        System.out.println(Test2.a);
+        System.out.println(Test1.b);
+
+
+    }
+}
---
+++ Note/class_def/app2.java
@@ -0,0 +1,12 @@
+// 還沒寫完
+
+class Test{
+
+    public static void main(String[] args) {
+        
+    }
+
+    static void func_1(){
+
+    }
+}
