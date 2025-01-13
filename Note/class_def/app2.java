9aa47c5314160d00c85aed013d2c34ba4ebfcc04
Du-Brandon <asdffhnkl123@gmail.com>
2025-01-13 19:59:22 +0800

    Add app2 in class_def

--- Note/class_def/app2.java
+++ Note/class_def/app2.java
@@ -3,10 +3,20 @@
 class Test{
 
     public static void main(String[] args) {
-        
+        Test.func_1();
+        Test.func_2("one");
     }
 
+    // 定義函式
     static void func_1(){
+        System.out.println("一二三四五");
+
+
+    }
+
+    // 定義參數
+    static void func_2(String a){
+        System.out.println(a);
 
     }
 }
