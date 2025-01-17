a1bfb81f321036f509c405128a2c4908b233db80
Du-Brandon <asdffhnkl123@gmail.com>
2025-01-17 16:55:27 +0800

    Example of package2

--- Note/package_/App.java
+++ Note/package_/App.java
@@ -1,4 +1,4 @@
-import math_.Basicmath;
+// import math_.Basicmath;
 
 class App{
     public static void main(String[] args) {
@@ -6,6 +6,8 @@ class App{
         System.out.println(math_.Basicmath.pi);
         int result = math_.Basicmath.add(8,9);
         System.out.println(result);
+
+        // System.out.println(Basicmath.pi); 有import才能調用這一行
     }
 }
 
